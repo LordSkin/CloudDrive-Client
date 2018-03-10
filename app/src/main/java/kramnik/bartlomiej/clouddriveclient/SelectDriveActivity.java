2e6103d0be7ed06b09413bf10858506757bfe648
LordSkin <bartek.kramnik@wp.pl>
2018-03-10 23:17:00 +0100

    Initial commit

---
+++ app/src/main/java/kramnik/bartlomiej/clouddriveclient/SelectDriveActivity.java
@@ -0,0 +1,13 @@
+package kramnik.bartlomiej.clouddriveclient;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SelectDriveActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_drive);
+    }
+}
